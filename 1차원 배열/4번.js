be5ef83ffd858d9662f3dd1e567c8972961ff90e
yejin <vgg789@naver.com>
2022-07-06 04:11:51 +0900

    feat: 백준 1차원 배열 - 4번

---
+++ 1차원 배열/4번.js
@@ -0,0 +1,29 @@
+let input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => Number(x));
+//let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(x => Number(x));
+
+//foreach와 indexof를 이용한 풀이
+let numArr = [];
+
+input.forEach((x) => {
+  const num = x % 42;
+
+  if (numArr.indexOf(num) === -1) {
+    // 없는 값이라면 -1를 반환
+    numArr.push(num); // 배열에 push
+  }
+});
+
+console.log(numArr.length);
+
+//set 이용한 풀이
+// let restArr = input.map(input => input % 42);
+
+// const set = new Set(restArr);
+// restArr = [...set];
+
+// console.log(restArr.length);
